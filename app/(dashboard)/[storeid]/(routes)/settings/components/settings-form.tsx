63e574a6b78b567a460485dc5380538ec20919d8
jai <data.jcthemax@gmail.com>
2023-12-20 13:04:39 +0900

    3:15

--- app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import * as z from "zod";
+import axios from "axios";
 import { useState } from "react";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -24,6 +28,9 @@ const formSchema = z.object({
 });
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+    const params = useParams();
+    const router = useRouter();
+
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
     
@@ -33,11 +40,26 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     });
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data);
+      try {
+        setLoading(true);
+        await axios.patch(`/api/stores/${params.storeId}`, data);
+        router.refresh();
+        toast.success("Store updated.");
+      } catch (error) {
+        toast.error("Something went wrong.");
+      } finally {
+        setLoading(false);
+      }
     };
 
   return (
     <>
+      <AlertModal 
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={() => { }}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
         <Button disabled={loading} variant="destructive" size="sm" onClick={() => setOpen(true)}>
